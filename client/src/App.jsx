d5005036670dc536afa05bda3bbcd262e01abb33
mann <pmannp0423@gmail.com>
2026-02-13 19:29:18 +0530

    f1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route, useLocation } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MoviesDetails from "./pages/MoviesDetails";
 import Seatlayout from "./pages/Seatlayout";
