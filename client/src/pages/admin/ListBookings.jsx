424959768b039d2d4cbad435b9c8d5409d9ba191
mann <pmannp0423@gmail.com>
2026-02-13 19:51:03 +0530

    fi

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { dummyBookingData } from "../../assets/assets";
 import Loading from "../../components/Loading";
-import Title from "../../components/admin/Title";
+import Title from "../../components/admin/title";
 import { dateFormat } from "../../lib/dateformat";
 import { useAppContext } from "../../context/AppContext";
 
